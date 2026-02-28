4be1e9f03ba29827dd35acbec2b6d1d169cdd94e
skyfires <chartchai.webdeveloper@gmail.com>
2026-02-28 22:13:29 +0700

    CM App development v1.0.8

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -53,7 +53,7 @@
               <button
                 type="button"
                 @click="showPassword = !showPassword"
-                class="absolute right-4 top-1/2 -translate-y-1/2 text-slate-400 hover:text-slate-600 transition-colors"
+                class="absolute right-4 top-1/2 -translate-y-1/2 text-slate-400 transition-colors pt-2"
               >
                 <Icon :name="showPassword ? 'lucide:eye-off' : 'lucide:eye'" size="20" />
               </button>
